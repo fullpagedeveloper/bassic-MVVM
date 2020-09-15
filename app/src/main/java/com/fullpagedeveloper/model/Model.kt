c19b12116c05c7c902765ef1a1d6a12f3fcd0db8
Maulana <maulana.ahmad@tsmid.com>
2020-09-16 01:48:48 +0700

    add class model

--- app/src/main/java/com/fullpagedeveloper/model/Model.kt
+++ app/src/main/java/com/fullpagedeveloper/model/Model.kt
@@ -1,31 +1,47 @@
 package com.fullpagedeveloper.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
     /**
      *  {
-                "bred_for": "Small rodent hunting, lapdog",
-                "breed_group": "Toy",
-                "height": {
-                "imperial": "9 - 11.5",
-                "metric": "23 - 29"
-            },
-                "id": 1,
-                "life_span": "10 - 12 years",
-                "name": "Affenpinscher",
-                "origin": "Germany, France",
-                "temperament": "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving",
-                "weight": {
-                "imperial": "6 - 13",
-                "metric": "3 - 6"
-            },
-            "url": "https://raw.githubusercontent.com/DevTides/DogsApi/master/1.jpg"
-        }
+    "bred_for": "Small rodent hunting, lapdog",
+    "breed_group": "Toy",
+    "height": {
+    "imperial": "9 - 11.5",
+    "metric": "23 - 29"
+    },
+    "id": 1,
+    "life_span": "10 - 12 years",
+    "name": "Affenpinscher",
+    "origin": "Germany, France",
+    "temperament": "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving",
+    "weight": {
+    "imperial": "6 - 13",
+    "metric": "3 - 6"
+    },
+    "url": "https://raw.githubusercontent.com/DevTides/DogsApi/master/1.jpg"
+    }
      */
+
+    @SerializedName("id")
     val breedId: String?, //pakai ? jika id bisa null atau kosong
+
+    @SerializedName("name")
     val dogBreed: String?,
+
+    @SerializedName("life_span")
     val lifeSpan: String?,
+
+    @SerializedName("breed_group")
     val breadGroup: String?,
+
+    @SerializedName("bred_for")
     val bredFor: String?,
+
+    @SerializedName("temperament")
     val temperament: String?,
+
+    @SerializedName("url")
     val imageUrl: String?
 )
